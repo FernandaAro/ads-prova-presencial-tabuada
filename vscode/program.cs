12be1db68d96e2cc0500d7c37132a6029dd27d66
Fernanda Aro <fernanda_aro@yahoo.com>
2023-11-27 19:12:40 -0300

    build: complete

---
+++ vscode/program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Prova
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Qual tabuada deseja executar?");
+            int tabuada = Convert.ToInt32(Console.ReadLine());
+
+            for (int n = 0; n <= 10; n++)
+            {
+                Console.WriteLine(tabuada + " x " + n + " = " + n * tabuada);
+            }
+        }
+    }
+}
